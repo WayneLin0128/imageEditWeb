7130e061080e3d4952844e7dbdf3f24ff7258d4f
waynelin0128 <waynelin0128@gmail.com>
2026-03-09 16:43:08 +0800

    Revert "Add HEIC upload conversion API"
    This reverts commit fbf8f32d193e453eaae7cf8a851f338de1e1dafc.

--- api/HeicConversionResult.cs
+++
@@ -1,9 +0,0 @@
-namespace ImageEditWeb.Api;
-
-public sealed record HeicConversionResult(bool Succeeded, byte[]? ImageBytes, string? ErrorCode, string? ErrorMessage)
-{
-    public static HeicConversionResult Success(byte[] imageBytes) => new(true, imageBytes, null, null);
-
-    public static HeicConversionResult Failure(string errorCode, string errorMessage) =>
-        new(false, null, errorCode, errorMessage);
-}
--- api/IHeicConversionService.cs
+++
@@ -1,6 +0,0 @@
-namespace ImageEditWeb.Api;
-
-public interface IHeicConversionService
-{
-    Task<HeicConversionResult> ConvertToJpegAsync(Stream input, CancellationToken cancellationToken);
-}
